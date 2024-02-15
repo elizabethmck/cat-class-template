d23f75e5c059b3014782dbf1411204a40119c534
elizabethmck <elizabeth.mckeeby@students.fhu.edu>
2024-02-15 13:15:04 -0600

    Update cat.py
    Test #2

--- cat.py
+++ cat.py
@@ -7,6 +7,15 @@ class Cat():
     def speak(self):
         print("Meow")
 
+stella = Cat()
+stella.name = "Stella"
+stella.age = 7
+
+garfield = Cat()
+garfield.name = "Garfield"
+garfield.age = 50
+
+
 #Create objects here
 #These should NOT be indented inside the class
 
